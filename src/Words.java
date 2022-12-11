1a985bd3052932b5e94c56e7dd45ba91581a5bc3
Kaleb Hings <30638410+kalebhings@users.noreply.github.com>
2022-12-10 19:29:00 -0700

    Initial commit

---
+++ src/Words.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Words {
+    private static final String[] words = {
+            "ABSTRACT", "ASSERT", "BOOLEAN", "BREAK", "BYTE",
+            "CASE", "CATCH", "CHAR", "CLASS", "CONST",
+            "CONTINUE", "DEFAULT", "DOUBLE", "DO", "ELSE",
+            "ENUM", "EXTENDS", "FALSE", "FINAL", "FINALLY",
+            "FLOAT", "FOR", "GOTO", "IF", "IMPLEMENTS",
+            "IMPORT", "INSTANCEOF", "INT", "INTERFACE",
+            "LONG", "NATIVE", "NEW", "NULL", "PACKAGE",
+            "PRIVATE", "PROTECTED", "PUBLIC", "RETURN",
+            "SHORT", "STATIC", "STRICTFP", "SUPER", "SWITCH",
+            "SYNCHRONIZED", "THIS", "THROW", "THROWS",
+            "TRANSIENT", "TRUE", "TRY", "VOID", "VOLATILE", "WHILE"
+    };
+
+    public static final Random random = new Random();
+
+    public String newWord() {
+        return words[random.nextInt(words.length)];
+    }
+
+}
